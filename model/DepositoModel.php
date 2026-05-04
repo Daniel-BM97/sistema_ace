9f3ac8e999830f496cffc21bd94e50c19c8fc5fb
Project_Endemias <lipe07barroso@gmail.com>
2026-05-04 17:42:14 -0300

    Add unit tests for DepositoModel and AreaController, fix DepositoModel namespace

--- model/DepositoModel.php
+++ model/DepositoModel.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace model;
+
 class DepositoModel {
     private $conn;
 
---
+++ tests/TesteDepositoModel.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use model\DepositoModel;
+
+class TesteDepositoModel extends TestCase
+{
+    public function testCadastrar(): void
+    {
+        $connMock = $this->createMock(mysqli::class);
+        $stmtMock = $this->createMock(mysqli_stmt::class);
+
+        $connMock->method('prepare')->willReturn($stmtMock);
+        $stmtMock->method('bind_param');
+        $stmtMock->method('execute')->willReturn(true);
+        $stmtMock->method('close');
+
+        $depositoModel = new DepositoModel($connMock);
+        $result = $depositoModel->cadastrar(1, 'A1', 1, 2.5);
+
+        $this->assertTrue($result);
+    }
+
+    public function testCadastrarMultiplos(): void
+    {
+        $connMock = $this->createMock(mysqli::class);
+        $stmtMock = $this->createMock(mysqli_stmt::class);
+
+        $connMock->method('prepare')->willReturn($stmtMock);
+        $stmtMock->method('bind_param');
+        $stmtMock->method('execute')->willReturn(true);
+        $stmtMock->method('close');
+
+        $depositoModel = new DepositoModel($connMock);
+        $depositos = [
+            ['a1' => 1, 'focos' => 2, 'larvicida' => 1.5],
+            ['a1' => 2, 'focos' => 0, 'larvicida' => 0]
+        ];
+        $result = $depositoModel->cadastrarMultiplos(1, $depositos);
+
+        $this->assertTrue($result);
+    }
+}
